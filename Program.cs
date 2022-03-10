6e8467146095cee986d6832b2bb47bdff3caf977
Saquib Anwar <89464459+saquib-anwar-zocdoc@users.noreply.github.com>
2022-03-10 15:56:10 +0530

    Changing pattern

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's start with the Singleton pattern!");
+
+            Singleton s1 = Singleton.Instance;
+            Singleton s2 = Singleton.Instance;
+            // Test for same instance
+            if (s1 == s2)
+            {
+                Console.WriteLine("Objects are the same instance\n");
+            }
+
+            Console.WriteLine("Let's start with the ThreadSafeSingleton pattern!");
+
+            ThreadSafeSingleton ts1 = ThreadSafeSingleton.Instance;
+            ThreadSafeSingleton ts2 = ThreadSafeSingleton.Instance;
+            // Test for same instance
+            if (ts1 == ts2)
+            {
+                Console.WriteLine("Objects are the same instance\n");
+            }
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,51 @@
+namespace DesignPatterns
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        private Singleton()
+        {
+        }
+        
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                }
+                return _instance;
+            }
+        }
+    }
+
+    public class ThreadSafeSingleton
+    {
+        private static ThreadSafeSingleton _instance;
+        private static readonly object _lock = new object();
+
+        private ThreadSafeSingleton()
+        {
+        }
+
+        public static ThreadSafeSingleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new ThreadSafeSingleton();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
