3a0b14ee8697576e52d47f11c08b0984c76a9ccc
Saquib Anwar <89464459+saquib-anwar-zocdoc@users.noreply.github.com>
2022-04-17 21:19:25 +0530

    Flyweight Design Pattern

---
+++ StructuralDesignPattern/Flyweight/Circle.cs
@@ -0,0 +1,11 @@
+namespace DesignPatterns
+{
+    //ConcreteFlyweight Class
+    public class Circle : IShape
+    {
+        public void Print()
+        {
+            System.Console.WriteLine("\nPrinting Circle");
+        }
+    }
+}
